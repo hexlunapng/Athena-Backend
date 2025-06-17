0b96baeb6d3a582189b836474fbd7a9bbd7781b7
Public-mrc <publicooo96@gmail.com>
2025-06-17 11:38:11 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	port := 3551
+	addr := ":" + strconv.Itoa(port)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Athena Backend Running On Port :3 %d", port)
+	})
+
+	fmt.Printf("Starting Athena Backend on %s\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
+}
