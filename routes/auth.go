ce541689f0bcb19b3d93dbca20f53af7f4f8081a
Luna <hexishexomgcute@gmail.com>
2025-06-17 06:51:02 -0400

    Update auth.go

--- routes/auth.go
+++ routes/auth.go
@@ -1,1 +1,23 @@
 package routes
+
+import ( 
+  "encoding/json"
+  "fmt"
+  "log"
+  "net/http"
+  "strings"
+  "time"
+"github.com/gorilla/mux"
+  "github.com/golang-jwt/jwt/v5"
+  "golang.org/x/crypto/bcrypt"
+)
+
+
+type user struct {
+  AccountID string
+  Email string
+  Username string
+  Password string
+  Banned bool
+  }
+
