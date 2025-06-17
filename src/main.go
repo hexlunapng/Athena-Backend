9fd6c09f662282f821a623acf4abae8ce22dd275
Public-mrc <publicooo96@gmail.com>
2025-06-17 11:43:53 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -6,15 +6,23 @@ import (
 	"strconv"
 )
 
+func colorize(text string, colorCode string) string {
+	reset := "\033[0m"
+	return colorCode + text + reset
+}
+
 func main() {
 	port := 3551
-	addr := ":" + strconv.Itoa(port)
+	addr := "127.0.0.1:" + strconv.Itoa(port)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Athena Backend Running On Port :3 %d", port)
 	})
+	brightCyan := "\033[96m"
+	tag := colorize("[Backend]", brightCyan)
 
-	fmt.Printf("Starting Athena Backend on %s\n", addr)
+	fmt.Printf("%s Starting Athena Backend on %s\n", tag, addr)
+
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		panic(err)
 	}
